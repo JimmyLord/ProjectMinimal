304b2eeadd3cc7e8f9d540a32c8d95a177b73e4d
Jimmy Lord <jimmylord@gmail.com>
2015-04-18 10:49:27 -0400

    Updated to latest framework and switched to a local wxwidgets project.

--- GameMinimalReplaceMe/Game/SourceCommon/Core/GameMinimalReplaceMe.cpp
+++ GameMinimalReplaceMe/Game/SourceCommon/Core/GameMinimalReplaceMe.cpp
@@ -1,25 +1,18 @@
 //
-// Copyright (c) 2012-2014 Jimmy Lord http://www.flatheadgames.com
+// Copyright (c) 2012-2015 Jimmy Lord http://www.flatheadgames.com
 //
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
+// This software is provided 'as-is', without any express or implied warranty.  In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
 #include "GameCommonHeader.h"
 
 GameMinimalReplaceMe::GameMinimalReplaceMe()
 {
-    m_pShader_VertexColor = 0;
+    m_pShaderFile_White = 0;
+    m_pShader_White = 0;
     m_pSprite = 0;
 
     m_Position.Set( 0, 0, 0 );
@@ -28,7 +21,9 @@ GameMinimalReplaceMe::GameMinimalReplaceMe()
 GameMinimalReplaceMe::~GameMinimalReplaceMe()
 {
     SAFE_RELEASE( m_pSprite );
-    SAFE_DELETE( m_pShader_VertexColor );
+
+    SAFE_RELEASE( m_pShader_White );
+    SAFE_RELEASE( m_pShaderFile_White );
 }
 
 void GameMinimalReplaceMe::OneTimeInit()
@@ -36,18 +31,18 @@ void GameMinimalReplaceMe::OneTimeInit()
     GameCore::OneTimeInit();
 
     // setup our shader.
-    m_pShader_VertexColor = MyNew ShaderGroup( MyNew Shader_Base(ShaderPass_Main), 0, 0 );
-    m_pShader_VertexColor->SetFileForAllPasses( "Data/Shaders/Shader_White" );
+    m_pShaderFile_White = RequestFile( "Data/Shaders/Shader_White.glsl" );
+    m_pShader_White = MyNew ShaderGroup( m_pShaderFile_White, "White" );
 
     // create a sprite, it's small since I'm not using a transform down below.
     m_pSprite = MyNew MySprite();
     m_pSprite->Create( 0.1f, 0.1f, 0, 1, 0, 1, Justify_Center, true );
-    m_pSprite->SetShader( m_pShader_VertexColor );
+    m_pSprite->SetShader( m_pShader_White );
 }
 
-void GameMinimalReplaceMe::Tick(double TimePassed)
+double GameMinimalReplaceMe::Tick(double TimePassed)
 {
-    GameCore::Tick( TimePassed );
+    return GameCore::Tick( TimePassed );
 }
 
 void GameMinimalReplaceMe::OnDrawFrame()
@@ -59,7 +54,7 @@ void GameMinimalReplaceMe::OnDrawFrame()
     // draw the sprite... no transform, so coords are in clip space.
     MyMatrix transform;
     transform.SetIdentity();
-    transform.SetPosition( m_Position );
+    transform.SetTranslation( m_Position );
 
     m_pSprite->Draw( &transform );
 }
--- GameMinimalReplaceMe/Game/SourceCommon/Core/GameMinimalReplaceMe.h
+++ GameMinimalReplaceMe/Game/SourceCommon/Core/GameMinimalReplaceMe.h
@@ -1,18 +1,10 @@
 //
-// Copyright (c) 2012-2014 Jimmy Lord http://www.flatheadgames.com
+// Copyright (c) 2012-2015 Jimmy Lord http://www.flatheadgames.com
 //
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
+// This software is provided 'as-is', without any express or implied warranty.  In no event will the authors be held liable for any damages arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
 #ifndef __GameMinimalReplaceMe_H__
@@ -21,7 +13,8 @@
 class GameMinimalReplaceMe : public GameCore
 {
 public:
-    ShaderGroup* m_pShader_VertexColor;
+    MyFileObject* m_pShaderFile_White;
+    ShaderGroup* m_pShader_White;
     MySprite* m_pSprite;
 
     Vector3 m_Position;
@@ -31,7 +24,7 @@ public:
     virtual ~GameMinimalReplaceMe();
 
     virtual void OneTimeInit();
-    virtual void Tick(double TimePassed);
+    virtual double Tick(double TimePassed);
     virtual void OnDrawFrame();
 
     virtual void OnTouch(int action, int id, float x, float y, float pressure, float size);
