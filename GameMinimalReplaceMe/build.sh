07120d2aecc379f7fdc88d840e3997cf3892cbbe
Jimmy Lord <jimmylord@gmail.com>
2017-08-29 07:58:06 -0400

    Cmake for multiple configurations, currently hard-coded for wx builds.

--- GameMinimalReplaceMe/build.sh
+++ GameMinimalReplaceMe/build.sh
@@ -1,11 +1,30 @@
-pushd ../Framework
-./build.sh
-popd
+
+if [[ ! $1 =~ ^(Debug|Release|wxDebug|wxRelease)$ ]]; then
+    echo "Specify a build configuration: Debug, Release, wxDebug, wxRelease"
+    exit 2
+fi
+
+BuildConfiguration="$1"
+
+echo "========== Configuration: $BuildConfiguration =========="
+
+pushd ../Framework > /dev/null
+    ./build.sh $BuildConfiguration
+popd > /dev/null
+
+echo "========== Building MinimalReplaceMe =========="
 
 if [ ! -d "build" ]; then
     mkdir build
-    cd build
-    cmake ..
 fi
-cd build
+
+if [ ! -d build/$BuildConfiguration ]; then
+    mkdir build/$BuildConfiguration
+    pushd build/$BuildConfiguration > /dev/null
+        cmake -DCMAKE_BUILD_TYPE=$BuildConfiguration ../..
+    popd > /dev/null
+fi
+
+pushd build/$BuildConfiguration > /dev/null
 make
+popd > /dev/null
