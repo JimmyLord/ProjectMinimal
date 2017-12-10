f55f964706bcc263f0a4f798ddc11ca67490f43e
Jimmy Lord <jimmylord@gmail.com>
2017-12-10 08:32:04 -0500

    Updated vscode settings and setup batch files.

--- Windows-CreateSymLinksForData.bat
+++ Windows-CreateSymLinksForData.bat
@@ -1,9 +1,5 @@
-@echo This batch file will link the game and engine data folders to the Android and NaCl build folders
+@echo This batch file will link the game data folder to the Android and NaCl build folders
 @echo --
-
-pushd GameEmptyReplaceMe\Game\Data
-mklink /J "DataEngine" "../../../Engine/DataEngine"
-popd
 
 pushd GameEmptyReplaceMe\Game\SourceAndroid\AndroidStudio\app\src\main
 mkdir assets
